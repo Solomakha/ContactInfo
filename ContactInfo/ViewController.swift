9650457fb88c81588147c41e599086a8f0b34159
Dmitry Solomaha <soldmich1995@gmail.com>
2022-10-13 13:29:24 +0300

    Сделал запрос на получение всех контактов из телефонной книги.  Получил доступ ко всем контактам.

--- ContactInfo/ViewController.swift
+++ ContactInfo/ViewController.swift
@@ -13,8 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchAllContacts()
     }
-
-
+    //Объявляю функцию которая будет получить все контакты с телефонной книги
+    func fetchAllContacts(){
+        //Получаю доступ к контактам
+        let contactStore = CNContactStore()
+        //Объявляю ключи которые буду получать из контактов
+        let keys = [CNContactGivenNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        //Запрос на получение контактов
+        let fetchRequest = CNContactFetchRequest(keysToFetch: keys)
+        //Вызов метода получения всех контактов
+        do {
+            try contactStore.enumerateContacts(with: fetchRequest, usingBlock: { contact, results in
+                //Операции над получеными контактами
+                print("\(contact.givenName)")
+            })
+        }
+        
+        catch {
+            //Вывод ошибки если что-то пошло не по плану
+            print("Something go wrong")
+        }
+    }
 }
 
